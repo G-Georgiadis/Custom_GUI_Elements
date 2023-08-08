ae3681200d56f1fdbf17b75c4929b2038a1a947d
George Georgiadis <60986897+G-Georgiadis@users.noreply.github.com>
2023-08-08 23:29:07 +0300

    Added Doxygen comments to WaveformButton_LookAndFeel

--- Source/LookAndFeels/WaveformButton_LookAndFeel.h
+++ Source/LookAndFeels/WaveformButton_LookAndFeel.h
@@ -3,10 +3,15 @@
 #include "JuceHeader.h"
 
 using namespace juce;
-
+/**
+ * LookAndFeel class for waveform selections buttons.
+ */
 class WaveformButton_LookAndFeel : public juce::LookAndFeel_V4
 {
 public:
+	/**
+	 * Waveform enum.
+	 */
 	enum Waveform
 	{
 		Sawtooth,
@@ -16,12 +21,31 @@ public:
 		Noise
 	};
 
+
+#pragma region Draw methods
+	/**
+* Draws the button background.
+*
+* \param Graphics& g: Reference to a graphics context used for drawing a component or image.
+* \param Button& button: Reference to the button to draw.
+* \param Colour backgroundColour: The colour to fill the background with.
+* \param Bool shouldDrawButtonAsHighlighted:True if the button should be draw.
+* \param Bool shouldDrawButtonAsDown:True if the button should appeat pressed.
+*/
 	void drawButtonBackground(Graphics& g, Button& button, const Colour& backgroundColour,
 		bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
 	{
 		g.fillAll(backgroundColour);
 	}
 
+	/**
+	 * Draws the body of the button.
+	 *
+	 * \param Graphics& g: Reference to a graphics context used for drawing a component or image.
+	 * \param Button& button: Reference to the button to draw.
+	 * \param Bool shouldDrawButtonAsHighlighted:True if the button should be draw.
+	 * \param Bool shouldDrawButtonAsDown:True if the button should appeat pressed.
+	 */
 	void drawButtonBody(Graphics& g, Button& button,
 		bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown)
 	{
@@ -32,10 +56,10 @@ public:
 		auto localHeight = localBounds.getHeight();
 
 		auto lightCenter = Point<float>(localX + localWidth * 0.8f, localY + localHeight * 0.2);
-		
+
 		auto darkPoint = Point<float>(localX + localWidth * 0.5f, localY + localHeight * 0.5f);
 
-		if (/*!shouldDrawButtonAsHighlighted && */!shouldDrawButtonAsDown)
+		if (!shouldDrawButtonAsDown)
 		{//Normal state
 			auto buttonBodyColourGradient = ColourGradient(normalButtonColour.brighter(0.2), lightCenter,
 				normalButtonColour, darkPoint, true);
@@ -53,6 +77,15 @@ public:
 		}
 	}
 
+	/**
+	 * Draws the shape of the given waveform.
+	 *
+	 * \param Graphics& g: A graphics context used for drawing a component or image.
+	 * \param Waveform waveform: The waveform to draw.
+	 * \param Rectangle localBounds:The space to draw the waveform in.
+	 * \param Bool shouldDrawButtonAsHighlighted:True if the button should be drawn highlighted. Not currently used.
+	 * \param Bool shouldDrawButtonAsDown:True if the button should appeat pressed.
+	 */
 	void drawWaveformShape(Graphics& g, Waveform waveform, Rectangle<int> localBounds, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown)
 	{
 		float strokeLineThickness = 2;
@@ -89,20 +122,37 @@ public:
 			break;
 		}
 		default:
+		{
+			g.strokePath(getSawtoothPath(localBounds.toFloat()), strokeType);
 			break;
 		}
+		}
 	}
+#pragma endregion
 
 
 #pragma region Waveform
 private:
+	/**
+	 * Instance of waveform enum.
+	 */
 	Waveform waveform;
 public:
+	/**
+	 * Sets the waveform to be drawn on this waveform button.
+	 * 
+	 * \param Waveform newWaveform: The waveform to set or change to.
+	 */
 	void setWaveform(Waveform newWaveform)
 	{
 		waveform = newWaveform;
 	}
 
+	/**
+	 * Returns the waveform assigned to the waveform button.
+	 * 
+	 * \return Waveform: The waveform assigned to the waveform button.
+	 */
 	Waveform getWaveform()
 	{
 		return waveform;
@@ -112,17 +162,63 @@ public:
 
 #pragma region Colour getters & setters
 public:
+	/**
+	 * Returns the background colour of this waveform button.
+	 * 
+	 * \return The background colour of this waveform button.
+	 */
 	Colour getButtonBackgroundColour() { return buttonBackgroundColour;	}
+
+	/**
+	 * Returns the normal colour (when not pressed or mouse-overed) of this waveform button.
+	 *
+	 * \return The normal  colour of this waveform button.
+	 */
 	Colour getNormalButtonColour() { return normalButtonColour; }
+
+	/**
+	 * Returns the over colour (when mouse-overed) of this waveform button.
+	 *
+	 * \return The over colour of this waveform button.
+	 */
 	Colour getOverButtonColour() { return overbuttonColour; }
+
+	/**
+	 * Returns the down colour (when pressed) of this waveform button.
+	 *
+	 * \return The down  colour of this waveform button.
+	 */
 	Colour getDownButtonColour() { return downButonColour; }
 
+
+	/**
+	 * Sets the background colour for this waveform button.
+	 * 
+	 * \param Colour& newColour: The colour to change the background to
+	 */
 	void setButtonBackgroundColour(Colour& newColour) { buttonBackgroundColour = newColour; }
+
+	/**
+	 * Sets the normal colour (when not pressed or mouse-overed) for this waveform button.
+	 *
+	 * \param Colour& newColour: The colour to change the normal colour  to
+	 */
 	void setNormalButtonColour(Colour& newColour) { normalButtonColour = newColour; }
+
+	/**
+	 * Sets the over colour (when mouse-overed) for this waveform button.
+	 *
+	 * \param Colour& newColour: The colour to change the over colour to
+	 */
 	void setOverButtonColour(Colour& newColour) { overbuttonColour = newColour; }
+
+	/**
+	 * Sets the down colour (when pressed) for this waveform button.
+	 *
+	 * \param Colour& newColour: The colour to change the down colour to
+	 */
 	void setDownButtonColour(Colour& newColour) { downButonColour = newColour; }
 #pragma endregion
-
 
 
 private:
@@ -130,8 +226,11 @@ private:
 	Colour buttonBackgroundColour = Colours::transparentBlack;
 	
 	/** Button colours */
+	/** Normal button colour */
 	Colour normalButtonColour = Colours::black.brighter(0.2);
+	/** Over (when mouse-overring) button colour */
 	Colour overbuttonColour = Colours::black.brighter(0.2);
+	/** Down (when pressed) button colour */
 	Colour downButonColour = Colours::black.brighter(0.6);
 
 	/** Shapes */
@@ -301,8 +400,9 @@ private:
 		else return noisePath;
 	}
 
+	/** The path of the shape for noise button. */
 	Path noisePath;
 
-
+	/** Instance of Random. */
 	Random random;
 };
