effd5f83acd9cbc7e9480a1b080203c2e0e1ec7c
George Georgiadis <60986897+G-Georgiadis@users.noreply.github.com>
2023-08-14 14:09:06 +0300

    Refactored  the gear shaped knob.

--- Source/LookAndFeels/RotaryKnob_GearShaped_LookAndFeel.h
+++ Source/LookAndFeels/RotaryKnob_GearShaped_LookAndFeel.h
@@ -69,9 +69,27 @@ public:
 		/** Knob outer circle. The outer gear shape edges should touch this circle. */
 		float knobGearOuterDiameter = 0.9f * totalDiameter;
 		float knobGearOuterRadius = knobGearOuterDiameter / 2.f;
-		/** Disk at the bottom of the knob */
-		//g.setColour(Colours::black.brighter(0.02));
-		ColourGradient backDiskGradient = ColourGradient(Colours::white.withAlpha(0.8f), totalCenterX + totalRadius, y, Colours::transparentBlack, totalCenterX, totalCenterY, false);
+
+		/** Knob inner circle. The inner gear shape edges should touch this circle. */
+		float knobGearInnerDiameter = 0.75 * totalDiameter;
+
+		/****************************************************************************
+		 * Disk at the bottom of the knob.
+		 */
+		 /** Base colour */
+		g.setColour(Colours::black.brighter(0.03));
+		g.fillEllipse
+		(
+			totalCenterX - knobGearOuterRadius,
+			totalCenterY - knobGearOuterRadius,
+			knobGearOuterDiameter,
+			knobGearOuterDiameter
+		);
+		/** Light reflection. */
+		ColourGradient backDiskGradient = ColourGradient(
+			Colours::white.withAlpha(0.15f), totalCenterX + knobGearOuterRadius / 2.f, totalCenterY - knobGearOuterRadius,
+			Colours::black.withAlpha(0.1f), totalCenterX - knobGearOuterRadius / 2.f, totalCenterY + knobGearOuterRadius,
+			true);
 		g.setGradientFill(backDiskGradient);
 		g.fillEllipse
 		(
@@ -82,38 +100,9 @@ public:
 		);
 
 
-		/** Knob inner circle. The inner gear shape edges should touch this circle. */
-		float knobGearInnerDiameter = 0.75 * totalDiameter;
-
-		/** Rircle to take an arc from */
-
-		float radiusOfCircleToTakeArcFrom = 0.25f * totalDiameter;
-		float circleToTakeArcFrom_CenterX = totalCenterX;
-		float circleToTakeArcFrom_CenterY = totalCenterY - knobGearInnerDiameter;
-		float circleToTakeArcFrom_Radius = knobGearInnerDiameter / 2.f;
-
-		constexpr float eighthOfCircleRadians = MathConstants<float>::pi / 4.f;
-		
-		Path gearShapedKnobPath;
-		gearShapedKnobPath.startNewSubPath(0, 0);
-		
-		for (int i = 0; i < numberOfSides; i++)
-		{
-			bool beginNewSubpath = (i == 0) ? true : false;
-			gearShapedKnobPath.addCentredArc(
-				circleToTakeArcFrom_CenterX,	//centerX
-				circleToTakeArcFrom_CenterY,	//centerY
-				circleToTakeArcFrom_Radius,	//radiusX
-				circleToTakeArcFrom_Radius,	//radiusY
-				0,	// Angle by which the whole ellipse should be rotated about its centre, in radians (clockwise)
-				3.5f * eighthOfCircleRadians,	// From radians
-				4.5f * eighthOfCircleRadians,	// To radians
-				beginNewSubpath);
-			gearShapedKnobPath.applyTransform(AffineTransform::rotation(eighthOfCircleRadians, totalCenterX, totalCenterY));
-		}
-		gearShapedKnobPath.closeSubPath();
-
-		/** Pointer */
+		/****************************************************************************
+		 * Pointer.
+		 */
 		Path knobPointerPath;
 		knobPointerPath.startNewSubPath(totalCenterX, y + (totalDiameter - knobGearOuterDiameter) / 2.f);
 		knobPointerPath.lineTo(totalCenterX + 0.15f * (knobGearOuterDiameter - knobGearInnerDiameter) / 2, y + (knobGearOuterDiameter - knobGearInnerDiameter));
@@ -128,31 +117,37 @@ public:
 		);
 
 		g.setColour(Colours::beige.darker(0.8));
-		g.fillPath(knobPointerPath);		
+		g.fillPath(knobPointerPath);
 
 
+		/****************************************************************************
+		 * Gear shaped knob.
+		 */
+		Path gearShapedKnobPath = getGearShapedPath(numberOfSides, totalDiameter, totalCenterX, totalCenterY, knobGearInnerDiameter);
+		
+		/** Rotate the knob depending on value (angle) */
 		gearShapedKnobPath.applyTransform(AffineTransform::rotation(
 			angle,
 			totalCenterX,
-			totalCenterY)
-		);
+			totalCenterY));
 		
+		DropShadow gearShadow = DropShadow(juce::Colours::black.withAlpha(0.3f), 20, juce::Point(-10, 10));
 		gearShadow.drawForPath(g, gearShapedKnobPath);
 		
-		ColourGradient gearTopGradient = ColourGradient(Colours::white.withAlpha(0.5f), totalCenterX + totalRadius, y, Colours::transparentBlack, totalCenterX, totalCenterY, false);
-		g.setGradientFill(gearTopGradient);
-
-		//g.setColour(Colours::black.brighter(0.05));
+		/** Knob base colour */
+		g.setColour(Colours::black.brighter(0.05));
 		g.fillPath(gearShapedKnobPath);
 
+		/** Gradient for light reflection effect. */
+		ColourGradient gearTopGradient = ColourGradient(
+			Colours::white.withAlpha(0.15f), totalCenterX + knobGearOuterRadius / 2.f, totalCenterY - knobGearOuterRadius,
+			Colours::black.withAlpha(0.1f), totalCenterX - knobGearOuterRadius / 2.f, totalCenterY + knobGearOuterRadius,
+			true);
+		g.setGradientFill(gearTopGradient);
+		g.fillPath(gearShapedKnobPath);
 	}
 
-	/**
-	 * Draws the text box of the rotary knob.
-	 * 
-	 * \param slider
-	 * \return 
-	 */
+	/** Draws the text box of the rotary knob. */
 	Label* createSliderTextBox(Slider& slider) override
 	{
 		/** Slider text box number of decimal places to display */
@@ -175,6 +170,35 @@ public:
 	
 private:
 	static constexpr int numberOfDecimalPlaces = 2;
-	DropShadow gearShadow = DropShadow(juce::Colours::black.withAlpha(0.3f), 20, juce::Point(-10, 10));
 	
+	/** Returns a gear shaped path */
+	Path getGearShapedPath(int numberOfSides, float totalDiameter, float totalCenterX, float totalCenterY, float knobGearInnerDiameter)
+	{
+		Path gearShapedKnobPath;
+		/** Circle to take an arc from */
+		float radiusOfCircleToTakeArcFrom = 0.25f * totalDiameter;
+		float circleToTakeArcFrom_CenterX = totalCenterX;
+		float circleToTakeArcFrom_CenterY = totalCenterY - knobGearInnerDiameter;
+		float circleToTakeArcFrom_Radius = knobGearInnerDiameter / 2.f;
+		constexpr float eighthOfCircleRadians = MathConstants<float>::pi / 4.f;
+		/** The gear shape will be created by drawing arcs, connecting them
+		 * (to make the outer flat bits) and then filling the inside. */
+		for (int i = 0; i < numberOfSides; i++)
+		{
+			bool beginNewSubpath = (i == 0) ? true : false;	// New path for the 1st arc only so that the rest are connected.
+			gearShapedKnobPath.addCentredArc(
+				circleToTakeArcFrom_CenterX,	//centerX
+				circleToTakeArcFrom_CenterY,	//centerY
+				circleToTakeArcFrom_Radius,	//radiusX
+				circleToTakeArcFrom_Radius,	//radiusY
+				0,	// Angle by which the whole ellipse should be rotated about its centre, in radians (clockwise)
+				3.5f * eighthOfCircleRadians,	// From radians
+				4.5f * eighthOfCircleRadians,	// To radians
+				beginNewSubpath);
+			gearShapedKnobPath.applyTransform(AffineTransform::rotation(eighthOfCircleRadians, totalCenterX, totalCenterY));
+		}
+		gearShapedKnobPath.closeSubPath();
+
+		return gearShapedKnobPath;
+	}
 };
